1e51fe8efcac4210746857ba920bce2e991706b2
Paolo Innocenti <paonath@gmail.com>
2019-05-28 23:23:28 +0200

    wip: REST client

---
+++ PH.Core3/PH.TinyRest/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using PH.Core3.Common.Result;
+
+namespace PH.TinyRest
+{
+    public interface IGet
+    {
+        Task<T> GetAsync<T>(string url);
+    }
+
+    public interface ICoreGet
+    {
+        Task<IResult<T>> GetResultAsync<T>(string url);
+    }
+
+    public class Client
+    {
+        private HttpClient _httpClient;
+
+        public Client()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public void P()
+        {
+            _httpClient.
+        }
+    }
+}
